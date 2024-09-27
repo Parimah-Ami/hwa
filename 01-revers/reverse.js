eab85e92475950ba7d4966fb8bc2ec56941fa3bc
Prima <parimah.amirian2000@gmail.com>
2024-09-27 19:33:01 +0330

    finished reverse js homework

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,10 @@
 // write your func here
+function reverseString (str){
+    let splitS = str.split ("");
+    console.log(splitS)
+    let reverseA = splitS.reverse();
+    console.log(reverseA)
+    let joinA = reverseA.join("");
+    console.log(joinA)
+}
+reverseString ("this will be a reverse string")
