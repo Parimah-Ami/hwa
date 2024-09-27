b0937d642c3fd34c72e17757810a28a0b21885b1
Prima <parimah.amirian2000@gmail.com>
2024-09-27 19:53:29 +0330

    factoriel homework done + find max modified

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,12 @@
 // write your func here
+function factorial(num) {
+    if (num < 0) 
+            return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorial(num - 1));
+    }
+    }
+    factorial(10);
+    console.log(factorial(10))
--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,2 @@
 // write your func here
+console.log(Math.max(1, 3, 2))
